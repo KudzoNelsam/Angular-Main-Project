25f410e80d821095f15e509c4b92f77ae0887d7a
KudzoNelsam <kudzo2004@gmail.com>
2025-05-15 16:56:06 +0000

    test de formulaire avec plusieurs niveaux

---
+++ src/app/core/environments/environments.development.ts
@@ -0,0 +1,3 @@
+export const ENVIRONMENTS = {
+    API_URL: "http://localhost:8080",
+};
---
+++ src/app/pages/securite/securite.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-securite',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './securite.component.html',
+  styleUrl: './securite.component.css'
+})
+export class SecuriteComponent {
+
+}
